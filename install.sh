0041b4819121b6e3f822f87f5c4257a22c74ea80
Andrew D <donegaan@tcd.ie>
2020-01-30 10:24:21 +0000

    Updated script to correctly download ns3 and auto yes apt-gets

--- install.sh
+++ install.sh
@@ -1,27 +1,29 @@
 #!/bin/bash
 
 # This script install all the required packages for The NS3DockerEmulator  https://github.com/chepeftw/NS3DockerEmulator
-# Ns3.26 and Docker 17.06
+# Ns3.30.1 and Docker 17.06
 # To running , open Terminal and execute
 # source install.sh
 
 echo -e "\n\n Updating enviroment... \n"
-sudo apt-get update
-sudo apt-get upgrade
-sudo apt-get dist-upgrade
+sudo apt-get -y update
+sudo apt-get -y upgrade
+sudo apt-get -y dist-upgrade
 
 echo -e "\n\n Installing git ... \n"
-sudo apt-get install git
+sudo apt-get -y install git
 
 echo -e "\n\n Installing Ns3 required packages ... \n"
-sudo apt-get install gcc g++ python python-dev mercurial bzr gdb valgrind gsl-bin libgsl2 libgsl2:i386 flex bison tcpdump sqlite sqlite3 libsqlite3-dev libxml2 libxml2-dev libgtk2.0-0 libgtk2.0-dev uncrustify doxygen graphviz imagemagick python-pygraphviz python-kiwi python-pygoocanvas libgoocanvas-dev python-pygccxml cmake autoconf libc6-dev libc6-dev-i386 g++-multilib texlive texlive-extra-utils texlive-latex-extra texlive-font-utils texlive-lang-portuguese dvipng python-pygraphviz python-kiwi python-pygoocanvas libgoocanvas-dev ipython libboost-signals-dev libboost-filesystem-dev openmpi-bin openmpi-common openmpi-doc libopenmpi-dev qt4-dev-tools libqt4-dev unzip p7zip-full unrar-free cvs
-sudo apt-get install python-pip
+sudo apt-get -y install gcc g++ python python-dev mercurial bzr gdb valgrind gsl-bin libgsl2 libgsl2:i386 flex bison tcpdump sqlite sqlite3 libsqlite3-dev libxml2 libxml2-dev libgtk2.0-0 libgtk2.0-dev uncrustify doxygen graphviz imagemagick python-pygraphviz python-kiwi python-pygoocanvas libgoocanvas-dev python-pygccxml cmake autoconf libc6-dev libc6-dev-i386 g++-multilib texlive texlive-extra-utils texlive-latex-extra texlive-font-utils texlive-lang-portuguese dvipng python-pygraphviz python-kiwi python-pygoocanvas libgoocanvas-dev ipython libboost-signals-dev libboost-filesystem-dev openmpi-bin openmpi-common openmpi-doc libopenmpi-dev qt4-dev-tools libqt4-dev unzip p7zip-full unrar-free cvs
+sudo apt-get -y install python-pip
 pip install pygccxml
 pip install --upgrade pip
 sudo -H pip install pygccxml --upgrade
 
 echo -e "\n\n Setting Ns3 workspace ... \n"
+cd
 git clone https://gitlab.com/nsnam/ns-3-allinone.git
+cd ns-3-allinone
 ./download.py
 
 echo -e "\n\n Verifying Ns3  ... \n"
@@ -40,16 +42,16 @@ cd ~
 
 echo -e "\n\n Installing Docker required packages  ... \n"
 
-sudo apt-get install apt-transport-https ca-certificates curl software-properties-common
+sudo apt-get -y install apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo apt-key fingerprint 0EBFCD88
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 
 service lxcfs stop
-sudo apt-get remove lxc-common lxcfs lxd lxd-client
+sudo apt-get -y remove lxc-common lxcfs lxd lxd-client
 
-sudo apt-get update
-sudo apt-get install docker-ce
+sudo apt-get -y update
+sudo apt-get -y install docker-ce
 
 echo -e "\n\n  Verifying  Docker  ... \n"
 sudo docker run hello-world
