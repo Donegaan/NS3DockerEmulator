04f37ddaca3742f8372f5ee0a15339a4b9cf8174
Andrew D <donegaan@tcd.ie>
2020-02-11 11:23:05 +0000

    Added waf vars back

--- ns3/tap-wifi-virtual-machine.cc
+++ ns3/tap-wifi-virtual-machine.cc
@@ -9,13 +9,12 @@
 
 using namespace ns3;
 
-int
-main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
   int NumNodes = 10;
   double TotalTime = 600.0;
 
-  double distance = 100;  // m
+  double distance = 100; // m
   int gridRowSize = 5;
 
   int nodeSpeed = 5; //in m/s
@@ -27,58 +26,56 @@ main (int argc, char *argv[])
   std::string TapBaseName = "emu";
 
   CommandLine cmd;
-  cmd.AddValue ("NumNodes", "Number of nodes/devices", NumNodes);
-  cmd.AddValue ("TotalTime", "Total simulation time", TotalTime);
-  cmd.AddValue ("TapBaseName", "Base name for tap interfaces", TapBaseName);
-  cmd.AddValue ("GridRowSize", "Grid row size", gridRowSize);
-  cmd.AddValue ("GridDistance", "Grid distance", distance);
-  cmd.AddValue ("SizeX", "Scenario Size in X axis", scenarioSizeX);
-  cmd.AddValue ("SizeY", "Scenario Size in Y axis", scenarioSizeY);
-  cmd.AddValue ("MobilityPause", "Mobility Pause", nodePause);
-  cmd.AddValue ("MobilitySpeed", "Mobility Speed", nodeSpeed);
+  cmd.AddValue("NumNodes", "Number of nodes/devices", NumNodes);
+  cmd.AddValue("TotalTime", "Total simulation time", TotalTime);
+  cmd.AddValue("TapBaseName", "Base name for tap interfaces", TapBaseName);
+  cmd.AddValue("GridRowSize", "Grid row size", gridRowSize);
+  cmd.AddValue("GridDistance", "Grid distance", distance);
+  cmd.AddValue("SizeX", "Scenario Size in X axis", scenarioSizeX);
+  cmd.AddValue("SizeY", "Scenario Size in Y axis", scenarioSizeY);
+  cmd.AddValue("MobilityPause", "Mobility Pause", nodePause);
+  cmd.AddValue("MobilitySpeed", "Mobility Speed", nodeSpeed);
 
-  cmd.Parse (argc,argv);
+  cmd.Parse(argc, argv);
 
   //
   // We are interacting with the outside, real, world.  This means we have to
   // interact in real-time and therefore means we have to use the real-time
   // simulator and take the time to calculate checksums.
   //
-  GlobalValue::Bind ("SimulatorImplementationType", StringValue ("ns3::RealtimeSimulatorImpl"));
-  GlobalValue::Bind ("ChecksumEnabled", BooleanValue (true));
-
+  GlobalValue::Bind("SimulatorImplementationType", StringValue("ns3::RealtimeSimulatorImpl"));
+  GlobalValue::Bind("ChecksumEnabled", BooleanValue(true));
 
   //
   // Create NumNodes ghost nodes.
   //
   NodeContainer nodes;
-  nodes.Create (NumNodes);
+  nodes.Create(NumNodes);
 
   //
   // We're going to use 802.11 A so set up a wifi helper to reflect that.
   //
   WifiHelper wifi;
-  wifi.SetStandard (WIFI_PHY_STANDARD_80211a);
-  wifi.SetRemoteStationManager ("ns3::ConstantRateWifiManager", "DataMode", StringValue ("OfdmRate54Mbps"));
+  wifi.SetStandard(WIFI_PHY_STANDARD_80211a);
+  wifi.SetRemoteStationManager("ns3::ConstantRateWifiManager", "DataMode", StringValue("OfdmRate54Mbps"));
 
   //
   // No reason for pesky access points, so we'll use an ad-hoc network.
   //
   WifiMacHelper wifiMac;
-  wifiMac.SetType ("ns3::AdhocWifiMac");
+  wifiMac.SetType("ns3::AdhocWifiMac");
 
   //
   // Configure the physcial layer.
   //
-  YansWifiChannelHelper wifiChannel = YansWifiChannelHelper::Default ();
-  YansWifiPhyHelper wifiPhy = YansWifiPhyHelper::Default ();
-  wifiPhy.SetChannel (wifiChannel.Create ());
+  YansWifiChannelHelper wifiChannel = YansWifiChannelHelper::Default();
+  YansWifiPhyHelper wifiPhy = YansWifiPhyHelper::Default();
+  wifiPhy.SetChannel(wifiChannel.Create());
 
   //
   // Install the wireless devices onto our ghost nodes.
   //
-  NetDeviceContainer devices = wifi.Install (wifiPhy, wifiMac, nodes);
-
+  NetDeviceContainer devices = wifi.Install(wifiPhy, wifiMac, nodes);
 
   // ++++++++++++++++++++++++++++++++++++
   // <Mobility>
@@ -93,58 +90,53 @@ main (int argc, char *argv[])
   ssPause << "" << nodePause << "s";
 
   std::stringstream ssBounds;
-  ssBounds << "0|" << scenarioSizeX << "|0|" << scenarioSizeY ;
+  ssBounds << "0|" << scenarioSizeX << "|0|" << scenarioSizeY;
 
   std::stringstream ssDiscPos;
-  ssDiscPos << "" << radio ;
+  ssDiscPos << "" << radio;
 
   std::stringstream ssRho;
-  ssRho << "ns3::UniformRandomVariable[Min=0|Max=" << radio << "]" ;
+  ssRho << "ns3::UniformRandomVariable[Min=0|Max=" << radio << "]";
 
+  Config::SetDefault("ns3::RandomWalk2dMobilityModel::Mode", StringValue("Time"));
+  Config::SetDefault("ns3::RandomWalk2dMobilityModel::Time", StringValue(ssPause.str()));
+  Config::SetDefault("ns3::RandomWalk2dMobilityModel::Speed", StringValue(ssSpeed.str()));
+  Config::SetDefault("ns3::RandomWalk2dMobilityModel::Bounds", StringValue(ssBounds.str()));
 
-    Config::SetDefault ("ns3::RandomWalk2dMobilityModel::Mode", StringValue ("Time"));
-    Config::SetDefault ("ns3::RandomWalk2dMobilityModel::Time", StringValue ( ssPause.str () ));
-    Config::SetDefault ("ns3::RandomWalk2dMobilityModel::Speed", StringValue ( ssSpeed.str () ));
-    Config::SetDefault ("ns3::RandomWalk2dMobilityModel::Bounds", StringValue ( ssBounds.str () ));
-
-    MobilityHelper mobility;
-    mobility.SetPositionAllocator ("ns3::RandomDiscPositionAllocator",
-                                 "X", StringValue ( ssDiscPos.str () ),
-                                 "Y", StringValue ( ssDiscPos.str () ),
-                                 "Rho", StringValue ( ssRho.str () ));
-    mobility.SetMobilityModel ("ns3::RandomWalk2dMobilityModel",
-                             "Mode", StringValue ("Time"),
-                             "Time", StringValue ( ssPause.str () ),
-                             "Speed", StringValue ( ssSpeed.str () ),
-                             "Bounds", StringValue ( ssBounds.str () ));
-
-
+  MobilityHelper mobility;
+  mobility.SetPositionAllocator("ns3::RandomDiscPositionAllocator",
+                                "X", StringValue(ssDiscPos.str()),
+                                "Y", StringValue(ssDiscPos.str()),
+                                "Rho", StringValue(ssRho.str()));
+  mobility.SetMobilityModel("ns3::RandomWalk2dMobilityModel",
+                            "Mode", StringValue("Time"),
+                            "Time", StringValue(ssPause.str()),
+                            "Speed", StringValue(ssSpeed.str()),
+                            "Bounds", StringValue(ssBounds.str()));
 
   // ++++++++++++++++++++++++++++++++++++
   // </Mobility>
   // ++++++++++++++++++++++++++++++++++++
 
-//  mobility.SetPositionAllocator (taPositionAlloc);
-  mobility.Install (nodes);
+  //  mobility.SetPositionAllocator (taPositionAlloc);
+  mobility.Install(nodes);
 
   TapBridgeHelper tapBridge;
-  tapBridge.SetAttribute ("Mode", StringValue ("UseLocal"));
+  tapBridge.SetAttribute("Mode", StringValue("UseLocal"));
 
   for (int i = 0; i < NumNodes; i++)
-    {
-        std::stringstream tapName;
-        tapName << "tap-" << TapBaseName << (i+1) ;
+  {
+    std::stringstream tapName;
+    tapName << "tap-" << TapBaseName << (i + 1);
 
-        tapBridge.SetAttribute ("DeviceName", StringValue (tapName.str ()));
-        tapBridge.Install (nodes.Get (i), devices.Get (i));
-    }
-
+    tapBridge.SetAttribute("DeviceName", StringValue(tapName.str()));
+    tapBridge.Install(nodes.Get(i), devices.Get(i));
+  }
 
   //
   // Run the simulation for TotalTime seconds to give the user time to play around
   //
-  Simulator::Stop (Seconds (TotalTime));
-  Simulator::Run ();
-  Simulator::Destroy ();
+  Simulator::Stop(Seconds(TotalTime));
+  Simulator::Run();
+  Simulator::Destroy();
 }
-
