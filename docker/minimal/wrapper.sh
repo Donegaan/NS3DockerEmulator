51b77c2738883b05489bd01e398d1ab6cb84f105
Andrew D <donegaan@tcd.ie>
2020-02-28 09:58:50 +0000

    Some testing using the beacon dockerfile

--- docker/minimal/wrapper.sh
+++ docker/minimal/wrapper.sh
@@ -15,61 +15,17 @@ echo "START" > /var/log/golang/wrapper.log
 echo "starting ... "
 echo "---------------------------------------------"
 
-#########################################################################################################
+
 # Start the first process
-echo "Starting RAFT ... " >> /var/log/golang/wrapper.log
-echo "/raft &" >> /var/log/golang/wrapper.log
+echo "Starting Beacon ... " >> /var/log/golang/wrapper.log
+echo "/beacon /beacon_conf/conf1.yml &" >> /var/log/golang/wrapper.log
 
-/raft &
+/beacon /beacon_conf/conf1.yml &
 status=$?
 if [ $status -ne 0 ]; then
-  echo "Failed to start raft process: $status"
-  echo "Failed to start raft process: $status" >> /var/log/golang/wrapper.log
+  echo "Failed to start beacon process: $status"
   exit $status
 fi
-
-
-#########################################################################################################
-# Start the second process
-echo "Starting Miner ... " >> /var/log/golang/wrapper.log
-echo "/miner &" >> /var/log/golang/wrapper.log
-
-/miner &
-status=$?
-if [ $status -ne 0 ]; then
-  echo "Failed to start miner process: $status"
-  echo "Failed to start miner process: $status" >> /var/log/golang/wrapper.log
-  exit $status
-fi
-
-
-#########################################################################################################
-# Start the third process
-echo "Starting Blockchain ... " >> /var/log/golang/wrapper.log
-echo "/blockchain &" >> /var/log/golang/wrapper.log
-
-/blockchain &
-status=$?
-if [ $status -ne 0 ]; then
-  echo "Failed to start blockchain process: $status"
-  echo "Failed to start blockchain process: $status" >> /var/log/golang/wrapper.log
-  exit $status
-fi
-
-
-#########################################################################################################
-# Start the fourth process
-echo "Starting Router ... " >> /var/log/golang/wrapper.log
-echo "/router &" >> /var/log/golang/wrapper.log
-
-/router &
-status=$?
-if [ $status -ne 0 ]; then
-  echo "Failed to start router process: $status"
-  echo "Failed to start router process: $status" >> /var/log/golang/wrapper.log
-  exit $status
-fi
-
 
 # Naive check runs checks once a minute to see if either of the processes exited.
 # This illustrates part of the heavy lifting you need to do if you want to run
@@ -77,45 +33,18 @@ fi
 # if it detects that either of the processes has exited.
 # Otherwise it will loop forever, waking up every 60 seconds
 
-sleep 5
-
 while /bin/true; do
 
-  ps aux | grep raft | grep -v grep
+  ps aux | grep beacon | grep -v grep
   P1_STATUS=$?
 
-  ps aux | grep miner | grep -v grep
-  P2_STATUS=$?
-
-  ps aux | grep blockchain | grep -v grep
-  P3_STATUS=$?
-
-  ps aux | grep router | grep -v grep
-  P4_STATUS=$?
-
-  echo "RAFT=${P1_STATUS}, MINER=${P2_STATUS}, BLOCKCHAIN=${P3_STATUS}, ROUTER=${P4_STATUS}" >> /var/log/golang/wrapper.log
+  echo "PROCESS1 STATUS = $P1_STATUS " >> /var/log/golang/wrapper.log
 
   # If the greps above find anything, they will exit with 0 status
   # If they are not both 0, then something is wrong
   if [ $P1_STATUS -ne 0 ]; then
-    echo "RAFT already exited."
-#    exit 1
+    echo "One of the processes has already exited."
+    exit -1
   fi
-
-  if [ $P2_STATUS -ne 0 ]; then
-    echo "MINER already exited."
-#    exit 1
-  fi
-
-  if [ $P3_STATUS -ne 0 ]; then
-    echo "BLOCKCHAIN already exited."
-#    exit 1
-  fi
-
-  if [ $P4_STATUS -ne 0 ]; then
-    echo "ROUTER already exited."
-#    exit 1
-  fi
-
   sleep 60
 done
