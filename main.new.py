fda51c8c50d45ea898b5f736d3bcfe049f36e9b5
Andrew D <donegaan@tcd.ie>
2020-02-28 11:49:01 +0000

    Wifi instead of csma

--- main.new.py
+++ main.new.py
@@ -171,7 +171,7 @@ def create():
                       baseContainerNameMin)
 
     r_code = subprocess.call(
-        "cd ns3 && bash update.sh tap-csma-virtual-machine-original.cc", shell=True)
+        "cd ns3 && bash update.sh tap-wifi-virtual-machine-original.cc", shell=True)
     if r_code != 0:
         print("Error copying latest ns3 file")
     else:
