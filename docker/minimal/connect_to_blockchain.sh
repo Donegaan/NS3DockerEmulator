a41d81b3acdf91fb4b8ff438de57497eeb3df77c
Andrew D <24252685+Donegaan@users.noreply.github.com>
2020-04-04 18:23:03 +0100

    Trying to create stream within script

--- docker/minimal/connect_to_blockchain.sh
+++ docker/minimal/connect_to_blockchain.sh
@@ -1,4 +1,11 @@
 # Address is ip:port, ip address seems to be the same everytime new container is created
 multichaind DockerChain@10.12.0.1:7447 -daemon
 
-python3 create_stream.py # Will run if producer
+multichain-cli DockerChain
+
+create stream stream1 '{"restrict":"write"}'
+
+quit
+
+# cat create_stream.py
+# python3 create_stream.py # Will run if producer
