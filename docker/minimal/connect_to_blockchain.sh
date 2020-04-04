45b91885fc2e2b03d9e823a8bc5cc86ec2acbc54
Andrew D <24252685+Donegaan@users.noreply.github.com>
2020-04-04 17:54:18 +0100

    Trying with create stream in connect to blockchain script

--- docker/minimal/connect_to_blockchain.sh
+++ docker/minimal/connect_to_blockchain.sh
@@ -1,2 +1,4 @@
 # Address is ip:port, ip address seems to be the same everytime new container is created
 multichaind DockerChain@10.12.0.1:7447 -daemon
+
+python3 create_stream.py # Will run if producer
