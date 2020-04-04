3e160898b0abbb6f81eeefab26a792db1364d09e
Andrew D <24252685+Donegaan@users.noreply.github.com>
2020-04-04 18:10:41 +0100

    Commented out producer connecting to own bchain

--- docker/minimal/connect_to_blockchain.sh
+++ docker/minimal/connect_to_blockchain.sh
@@ -1,4 +1,4 @@
 # Address is ip:port, ip address seems to be the same everytime new container is created
-multichaind DockerChain@10.12.0.1:7447 -daemon
+# multichaind DockerChain@10.12.0.1:7447 -daemon # Don't need to run this if producer
 
 python3 create_stream.py # Will run if producer
